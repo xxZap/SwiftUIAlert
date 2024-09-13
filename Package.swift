f3ff3c850f2933e909039674c768637915708069
Alessio Zap Boerio <alessio.boerio@igenius.ai>
2024-09-13 16:07:00 +0200

    Remove support for macOS

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIAlert",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v11)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
