4a4519700b09db4c79f921b7205b075ea551475c
Alessio Zap Boerio <boerioalessio@gmail.com>
2025-02-14 16:09:33 +0100

    Enable macOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIAlert",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
